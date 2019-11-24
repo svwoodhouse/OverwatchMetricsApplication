478785a9656329cdc733e07df738b3fbc77db1f0
svwoodhouse <svwoodhouse@gmail.com>
2019-11-24 12:52:53 -0500

    Finished pulling data from individual heroes. Created a dictonary for hero data. Need to add remaining heroes to the dictonary list

--- webscraper.py
+++ webscraper.py
@@ -11,7 +11,7 @@ soup2 = bs(soup.prettify(),"html.parser")
 winstonDataFile = open(r"C:\Users\Sydnee\Desktop\OverwatchWebScraper\WinstonData.txt", "w+")
 #Character IDs
 #All Heroes = 0x02E00000FFFFFFFF
-#Ana = 0x02E00000FFFFFFFF
+#Ana = 0x02E000000000013B
 #Ashe = 0x02E0000000000200
 #Baptiste = 0x02E0000000000221
 #Bastion = 0x02E0000000000015
@@ -60,23 +60,30 @@ winstonDataFile = open(r"C:\Users\Sydnee\Desktop\OverwatchWebScraper\WinstonData
 
 #This code gets the stats from Winston
 header = True
-winston = "0x02E0000000000009"
-example = soup.findAll("div",{"data-category-id":winston})
-for e in example:
-   data = e.findAll("td",{"class":"DataTable-tableColumn"})
-   for d in data:
-      if header == True:
-         title = d.get_text()
-         print(d.get_text()+ " ---------> ", end="")
-         winstonDataFile.write(title + " ---------> ")
-         header = False
-      else:
-         stats = d.get_text()
-         print(stats +"\n")
-         header = True
-         winstonDataFile.write(stats + "\n")
 
-#winstonDataFile.close()
+characters = {'Ana' : '0x02E000000000013B',
+                'Ashe' : '0x02E0000000000200'}
+                
+winston = "0x02E0000000000009"
+for characterName, characterID in characters.items():
+   winstonDataFile.write(characterName +" character Data"+ "\n")
+   example = soup.findAll("div",{"data-category-id":characterID})
+   for e in example:
+      data = e.findAll("td",{"class":"DataTable-tableColumn"})
+      for d in data:
+         if header == True:
+            title = d.get_text()
+            print(d.get_text()+ " ---------> ", end="")
+            winstonDataFile.write(title + " ---------> ")
+            header = False
+         else:
+            stats = d.get_text()
+            print(stats +"\n")
+            header = True
+            winstonDataFile.write(stats + "\n")
+   winstonDataFile.write("\n")
+
+winstonDataFile.close()
 
 #bestData = soup.findAll("div", {"class":"column xs-12 md-6 xl-4"})
 #for x in bestData:
