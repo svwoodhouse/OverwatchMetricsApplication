5000b6eedb4133b8cd43d59f498091d28580fa62
Sydnee <svwoodhouse@gmail.com>
2023-12-03 21:41:40 -0500

    Added updates to get data

--- webscraper.py
+++ webscraper.py
@@ -1,47 +1,14 @@
-#This code compares the stats of two Overwatch Players to see who is better in each category for the different characters in the game
-#Character IDs
-#All Heroes = 0x02E00000FFFFFFFF
-#Ana = 0x02E000000000013B
-#Ashe = 0x02E0000000000200
-#Baptiste = 0x02E0000000000221
-#Bastion = 0x02E0000000000015
-#Brigitte = 0x02E0000000000195
-#D.Va = 0x02E000000000007A
-#Doomfist = 0x02E000000000012F
-#Genji = 0x02E0000000000029
-#Hanzo = 0x02E0000000000005
-#Junkrat = 0x02E0000000000065
-#Lucio = 0x02E0000000000079
-#McCree = 0x02E0000000000042
-#Mei = 0x02E00000000000DD
-#Mercy = 0x02E0000000000004
-#Moira = 0x02E00000000001A2
-#Orisa = 0x02E000000000013E
-#Pharah = 0x02E0000000000008
-#Reaper = 0x02E0000000000002
-#Reinhardt = 0x02E0000000000007
-#Roadhog = 0x02E0000000000040
-#Sigma = 0x02E000000000023B
-#Soldier:76 = 0x02E000000000006E
-#Sombra = 0x02E000000000012E
-#Symmetra = 0x02E0000000000016
-#Torbjorn = 0x02E0000000000006
-#Tracer = 0x02E0000000000003
-#Widowmaker = 0x02E000000000000A
-#Winston = 0x02E0000000000009
-#Wrecking Ball = 0x02E00000000001CA
-#Zarya = 0x02E0000000000068
-#Zenyatta = 0x02E0000000000020
-
 #Library Imports
 from bs4 import BeautifulSoup as bs
 import requests
 import pandas as pd
 import matplotlib.pyplot as plt
+import itertools 
+
 pd.options.mode.chained_assignment = None 
 #URL for the Players Data
-url = "https://playoverwatch.com/en-us/career/pc/takaharimi-1252"
-bennyURL = "https://playoverwatch.com/en-us/career/pc/blin1343-1104"
+url = "https://overwatch.blizzard.com/en-us/career/takaharimi-1252/"
+bennyURL = "https://overwatch.blizzard.com/en-us/career/blin1343-1104/"
 
 #This grabs the HTML content of the players profile 
 header = {"User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36"}
@@ -52,45 +19,37 @@ soup = bs(page.content, "html.parser")
 bennySoup = bs(bennyPage.content, "html.parser")
 
 #Text Files containing players Data
-characterDataFile = open(r"C:\Users\Sydnee\Desktop\OverwatchWebScraper\characterData.txt", "w+")
+characterDataFile = open(r"characterData.txt", "w+")
 
 #Adding the headers to the file
 #characterDataFile.write("Hero, Category, Stat, Player\n")
 
 #This function gathers the stats from the different players 
 header = True
-characters = {'All Heroes' : '0x02E00000FFFFFFFF',
-              'Ana' : '0x02E000000000013B',
-              'Ashe' : '0x02E0000000000200', 
-              'Baptiste' : '0x02E0000000000221',
-              'Bastion' : '0x02E0000000000015',
-              'Brigitte' : '0x02E0000000000195',
-              'D.Va' : '0x02E000000000007A',
-              'Doomfist' : '0x02E000000000012F',
-              'Genji' : '0x02E0000000000029',
-              'Hanzo' : '0x02E0000000000005',
-              'Junkrat' : '0x02E0000000000065',
-              'Lucio' : '0x02E0000000000079',
-              'McCree' : '0x02E0000000000042',
-              'Mei' : '0x02E00000000000DD',
-              'Mercy' : '0x02E0000000000004',
-              'Moira' : '0x02E00000000001A2',
-              'Orisa' : '0x02E000000000013E',
-              'Pharah' : '0x02E0000000000008',
-              'Reaper' : '0x02E0000000000002',
-              'Reinhardt' : '0x02E0000000000007',
-              'Roadhog' : '0x02E0000000000040',
-              'Sigma' : '0x02E000000000023B',
-              'Soldier:76' : '0x02E000000000006E',
-              'Sombra' : '0x02E000000000012E',
-              'Symmetra' : '0x02E0000000000016',
-              'Torbjorn' : '0x02E0000000000006',
-              'Tracer' : '0x02E0000000000003',
-              'Widowmaker' : '0x02E000000000000A',
-              'Winston' : '0x02E0000000000009',
-              'Wrecking Ball' : '0x02E00000000001CA',
-              'Zarya' : '0x02E0000000000068',
-              'Zenyatta' : '0x02E0000000000020'}
+
+dataCatagoryID = {}
+data=[]
+
+def getDataCatagoryID():
+   getMainDiv = soup.findAll("div",{"class":"Profile-heroSummary--view quickPlay-view is-active"})
+   for dataCatagories in getMainDiv:
+      catagories = dataCatagories.findAll("option")
+      for c in catagories:
+         catagory = c.get_text()
+         catagoryID = c['value']
+         dataCatagoryID[catagoryID] = catagory
+
+def getStats(optionID):
+   getMainDiv = soup.findAll("div",{"class":"Profile-heroSummary--view quickPlay-view is-active"})
+   for dataCatagoryDiv in getMainDiv: 
+      getDataCatagoryDiv = soup.findAll("div",{"data-category-id":optionID})
+      for characters in getDataCatagoryDiv:
+         characterName = characters.findAll("div", {"class": "Profile-progressBar-title"})
+         characterStat = characters.findAll("div", {"class": "Profile-progressBar-description"})
+         for c, d in zip(characterName, characterStat):
+            name = c.get_text()
+            stat= d.get_text()
+            data.append({"name":name, "title": dataCatagoryID.get(optionID), "stat": stat})
 
 def gatherStats(header, characters, soup, playerName):
    for characterName, characterID in characters.items():
@@ -110,16 +69,20 @@ def gatherStats(header, characters, soup, playerName):
                characterDataFile.write(stats + "," + playerName + "\n")
          #print(characterName + ", " + title + ", " + stats + ", " + playerName + "\n")
 
-gatherStats(header,characters,soup,"takaharimi")
-gatherStats(header,characters,bennySoup,"blin1343")
-characterDataFile.close()
-Overwatch_Stats = pd.read_csv("C:/Users/Sydnee/Desktop/OverwatchWebScraper/characterData.txt",sep=",", encoding ='unicode_escape', names=["Hero","Category", "Stat", "Player"])
-#print (Overwatch_Stats)
-Medals = Overwatch_Stats[Overwatch_Stats["Category"] == "Medals"]
-Medals["Stat"] = Medals["Stat"].astype(str).astype(int)
-#print(Medals["Stat"])
-Zen = Medals[Medals["Hero"] == "Zenyatta"]
-#Zen["Stat"].astype("int32").dtypes
-Zen.plot(kind = "bar", y="Stat", x="Player")
-#print (list(Overwatch_Stats))
-plt.show()
+getDataCatagoryID()
+for i,id in enumerate(dataCatagoryID):
+   getStats(id)
+
+# (header,characters,soup,"takaharimi")
+# gatherStats(header,characters,bennySoup,"blin1343")
+# characterDataFile.close()
+# Overwatch_Stats = pd.read_csv("characterData.txt",sep=",", encoding ='unicode_escape', names=["Hero","Category", "Stat", "Player"])
+# #print (Overwatch_Stats)
+# Medals = Overwatch_Stats[Overwatch_Stats["Category"] == "Medals"]
+# Medals["Stat"] = Medals["Stat"].astype(str).astype(int)
+# #print(Medals["Stat"])
+# Zen = Medals[Medals["Hero"] == "Zenyatta"]
+# #Zen["Stat"].astype("int32").dtypes
+# Zen.plot(kind = "bar", y="Stat", x="Player")
+# #print (list(Overwatch_Stats))
+# plt.show()
